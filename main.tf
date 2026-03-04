d7083754ca4e07f961976f05056a2cbe8e14d6a8
Aymogul <aymogul22@gmail.com>
2026-03-04 13:14:19 +0100

    route table association

--- main.tf
+++ main.tf
@@ -21,8 +21,8 @@ resource "aws_subnet" "myapp-subnet-1" {
 
 resource "aws_route_table" "myapp-route-table" {
     vpc_id = aws_vpc.myapp-vpc.id
-
-    route = {
+    
+    route {
         cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.myapp-igw.id
     }
@@ -39,6 +39,11 @@ resource "aws_internet_gateway" "myapp-igw" {
   
 }
 
+resource "aws_route_table_association" "myapp-route-table-association" {
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    route_table_id = aws_route_table.myapp-route-table.id
+  
+}
 #data "aws_vpc" "existing_vpc" 
 #    default = true 
 #    tags = {
