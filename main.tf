19a0487e20885947c20c11e10d9b19a861dafde7
Aymogul <aymogul22@gmail.com>
2026-03-04 12:02:59 +0100

    starting with tenacity

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "aws" {}
+
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block       = var.vpc_cidr_block
+
+  tags = {
+    Name = "development-vpc"
+  }
+}
+resource "aws_subnet" "myapp-subnet-1" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    cidr_block = var.subnet_cidr_block
+    availability_zone = var.avail_zone
+    tags = {
+        Name = "myapp-subnet-1"
+    }
+  
+}
+
+#data "aws_vpc" "existing_vpc" {
+#    default = true 
+#    tags = {
+#        Name = "default"      
+#   }
+#}
+
+#resource "aws_subnet" "practice-2" {
+#    vpc_id = data.aws_vpc.existing_vpc.id
+#    cidr_block = "172.31.96.0/20"
+#    availability_zone = "us-east-1a"
+#    tags = {
+#        Name = "default-subnet"
+#    }
+#}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_cidr_block" {
+  
+}
+variable "subnet_cidr_block" {
+  
+}
+variable "avail_zone" {
+  
+}
