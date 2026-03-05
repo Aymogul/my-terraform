d333e95b6f0f3d605e4c5604428bd96cfaf0d889
Aymogul <aymogul22@gmail.com>
2026-03-05 12:19:55 +0100

    my ip address

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ resource "aws_security_group" "myapp-sg" {
     from_port = 22
     to_port   = 22
     protocol  = "tcp"
-    cidr_blocks = [  ]
+    cidr_blocks = var.my_ip
   }
   egress {
     from_port = 0
