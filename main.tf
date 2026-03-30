6a1d550460d19ebedfa7e83dc927eb62b80bcb30
Aymogul <aymogul22@gmail.com>
2026-03-30 12:47:28 +0100

    new docker line

--- main.tf
+++ main.tf
@@ -137,7 +137,8 @@ resource "aws_instance" "myapp-webapp" {
                   #!/bin/bash
                   sudo yum update -y && sudo yum install -y docker
                   sudo systemctl start docker
-  
+                  sudo usermod -aG docker ec2-user
+                  docker run -p 8080:80 nginx
               EOF
   tags = {
     Name = "${var.env_prefix}-server"
